6a61ff25e39ea29ab15e926a1f2f4545e4586b4f
10xRyan <yueren0429@outlook.com>
2023-08-03 00:28:55 +0800

    data transformed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts','train.csv')
@@ -31,6 +34,15 @@ class DataIngestion:
 
             frames = [bmw,audi,ford,hyundi,benz,toyota]
             merged = pd.concat(frames)
+
+            #Edge Case
+            duplicates_mask = merged.duplicated(subset='model', keep=False)
+            unique_models_df = merged[~duplicates_mask]
+            once=unique_models_df['model'].tolist()
+            for i in once:
+                merged.drop(merged[merged['model']==i].index,inplace=True)
+            #
+                
             df = merged.drop_duplicates()
             logging.info("Read the datasets as dataframes")
 
@@ -55,4 +67,7 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
