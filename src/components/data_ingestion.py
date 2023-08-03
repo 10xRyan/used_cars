fc035357df3ac7b74d39ab9b4f378f03adebbc63
10xRyan <yueren0429@outlook.com>
2023-08-03 21:55:35 +0800

    model trained

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,6 +9,7 @@ from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -70,4 +71,8 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    trainer=ModelTrainer()
+    result=trainer.initiate_model_trainer(train_arr,test_arr)
+    print("Best Model:",result[0],"R2:",result[1],"MAE:",result[2])
--- src/utility.py
+++ src/utility.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 
 from src.exception import CustomException
+from sklearn.metrics import r2_score,mean_absolute_error
 
 def save_object(file_path, obj):
     try:
@@ -17,3 +18,32 @@ def save_object(file_path, obj):
     
     except Exception as e:
         raise CustomException(e,sys)
+
+def evaluate_models(X_train, y_train,X_test,y_test,models):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            #para=param[list(models.keys())[i]]
+
+            #gs = GridSearchCV(model,para,cv=3)
+            #gs.fit(X_train,y_train)
+
+            #model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_test_pred = model.predict(X_test)
+
+            #!Criteria for selecting the best model: mea aboslute error (could be changed)
+            #Needs to change the method to min() in model trainer
+            test_model_score = mean_absolute_error(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
